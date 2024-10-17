de998d2c51c984c08156ce24b72525524838667a
MarcosViniSil <marcossilv203@gmail.com>
2024-10-17 17:06:43 -0300

    fix: ajust .bat to delete file correctly

--- startDocker.bat
+++ startDocker.bat
@@ -1,14 +1,14 @@
-
- @echo off
+@echo off
 
 set "PASTA=mysql_data"
 
+docker-compose down -v
+
 if exist "%PASTA%" (
+    echo Excluindo pasta %PASTA%...
     rmdir /S /Q "%PASTA%"
 ) else (
     echo Pasta %PASTA% não encontrada.
 )
 
 docker-compose up -d
-
-
